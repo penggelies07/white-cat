827f004e7f09e9827ac5e9090dda51c93c49cf09
varHarrie <varharrie@gmail.com>
2017-07-11 15:52:43 +0800

    update: simplify Base component

--- src/libs/Base.tsx
+++ src/libs/Base.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react'
 import * as classnames from 'classnames'
 
-interface IClassDictionary {
-  [id: string]: boolean | undefined | null
-}
-
+// classnames declarations
+interface IClassDictionary {[id: string]: boolean | undefined | null}
 interface IClassArray extends Array<ClassValue> {}
-
 type ClassValue = string | number | IClassDictionary | IClassArray[] | undefined | null | false
-
-interface IElementData {
-  [name: string]: string
-}
 
 interface IBaseProps {
   className?: string,
@@ -21,10 +14,6 @@ interface IBaseProps {
 export default abstract class Base<P = {}, S = {}> extends React.Component<P & IBaseProps, S> {
   mounted: boolean = false
 
-  constructor (props: any) {
-    super(props)
-  }
-
   componentDidMount () {
     this.mounted = true
   }
@@ -33,17 +22,11 @@ export default abstract class Base<P = {}, S = {}> extends React.Component<P & I
     this.mounted = false
   }
 
-  rootProps = (classes: ClassValue[] | ClassValue, styles?: React.CSSProperties, data?: IElementData) => {
-    const {className, style, ...rest} = this.props as any
-    const props: any = {
+  rootProps = (classes: ClassValue[] | ClassValue, styles?: React.CSSProperties) => {
+    const {className, style} = this.props
+    return {
       className: classnames(classes, className),
       style: Object.assign({}, styles, style)
     }
-    for (let name in rest) {
-      if (name.startsWith('data-')) {
-        props[name] = rest[name]
-      }
-    }
-    return props
   }
 }
